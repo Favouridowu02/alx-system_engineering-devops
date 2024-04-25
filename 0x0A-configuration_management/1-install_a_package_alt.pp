db3b30cc14fbc6df20ae3ecc2cdb8349b6957a5f
Favour Idowu <favouridowu02@gmail.com>
2024-04-25 10:49:23 +0100

    A puppet Script that installs the flask package

--- 0x0A-configuration_management/1-install_a_package_alt.pp
+++ 0x0A-configuration_management/1-install_a_package_alt.pp
@@ -1,4 +1,8 @@
 # To install Flask package
+exec { 'apt update':
+  command     => '/usr/bin/apt update',
+  refreshonly => true,
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',

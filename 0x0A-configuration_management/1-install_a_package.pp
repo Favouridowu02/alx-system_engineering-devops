9b7ab8e50c57c77a0fed3f9403a385e6d494c406
Favour Idowu <favouridowu02@gmail.com>
2024-04-25 12:16:25 +0100

    A puppet script to add flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # To install Flask package
 
-exec { "install flask":
-  command     => "/usr/bin/pip3 install flask==2.1.0 werkzeug==2.1.1",
+exec { 'install flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.1.1',
 }

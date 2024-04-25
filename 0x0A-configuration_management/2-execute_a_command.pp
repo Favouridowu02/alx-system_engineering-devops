9189a99702971f898500d1db27acd470e4fc2d61
Favour Idowu <favouridowu02@gmail.com>
2024-04-25 13:23:06 +0100

    Puppet Script to terminate a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
 # A Manifest that kills a process named killmenow
-exec { 'pkill killmenow':
-  command => '/usr/bin/pkill killmenow',
+$process_name='killmenow'
+exec { 'pkill $process_name':
+  command => "/usr/bin/pkill $process_name",
 }

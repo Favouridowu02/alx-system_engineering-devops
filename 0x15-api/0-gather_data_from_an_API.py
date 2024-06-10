c948ef8028b8e5c9c9f8da427f525a13585fe91d
Favour Idowu <favouridowu02@gmail.com>
2024-06-10 20:22:15 +0100

    Just added a file that gets from an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
 from sys import argv
-import  requests
+import requests
 
 if __name__ == "__main__":
     """This is """

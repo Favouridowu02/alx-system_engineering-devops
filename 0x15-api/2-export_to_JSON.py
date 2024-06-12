20012a985c3b932e327e0c5c5296c01ff729dcde
Favour Idowu <favouridowu02@gmail.com>
2024-06-12 19:48:29 +0100

    just wrote a script that gets data from an api and redirects the information to a Json file with a specific format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,13 +5,17 @@ import json
 import requests
 
 if __name__ == "__main__":
-    """ This Section Of Code is Only Runnable when Not inported into another Module """
+    """
+        This Section Of Code is Only Runnable when Not inported
+        into another Module
+    """
     uri = "https://jsonplaceholder.typicode.com/"
     user = requests.get(uri + "users/{}".format(argv[1])).json()
     todos = requests.get(uri + "todos", params={"userId": argv[1]}).json()
     json_my = {argv[1]: []}
     for todo in todos:
-        use = {"task": todo["title"], "completed": todo["completed"], "username": user["username"]}
+        use = {"task": todo["title"], "completed": todo["completed"],
+               "username": user["username"]}
         json_my[argv[1]].append(use)
     with open(argv[1] + ".json", 'w') as json_file:
         json_file.write(json.dumps(json_my))

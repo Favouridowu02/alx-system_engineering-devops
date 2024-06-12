8852c782280a9362ed2e684cdc97222d1e800313
Favour Idowu <favouridowu02@gmail.com>
2024-06-12 19:46:11 +0100

    just wrote a script that gets data from an api and redirects the information to a Json file with a specific format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This Module Contains Some Script that exports User Data using a Format """
+from sys import argv
+import json
+import requests
+
+if __name__ == "__main__":
+    """ This Section Of Code is Only Runnable when Not inported into another Module """
+    uri = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(uri + "users/{}".format(argv[1])).json()
+    todos = requests.get(uri + "todos", params={"userId": argv[1]}).json()
+    json_my = {argv[1]: []}
+    for todo in todos:
+        use = {"task": todo["title"], "completed": todo["completed"], "username": user["username"]}
+        json_my[argv[1]].append(use)
+    with open(argv[1] + ".json", 'w') as json_file:
+        json_file.write(json.dumps(json_my))

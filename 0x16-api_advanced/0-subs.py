1d32dfe65149873b8059acb493d0c39ff06ebad5
Favour Idowu <favouridowu02@gmail.com>
2024-08-13 17:09:15 +0100

    Add script to retrieve Reddit subscribers
    Implemented a Python Script that uses Reddits's API to fetch the number of subscribers for a given subreddit. The script includes error handling and can be used by importing
    the function number_of_subscribers from the module

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    This Module contains a function That queries the reddit API
+    and returns the number of subscribers.
+
+    Function_name: name_of_subscribers.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+        This function queries the reddit api and tries to list the
+        total amount of subscribers. It retunrs zero if no
+        subrredit is given
+
+        Arguments:
+            subreddit: this is the subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json"
+    headers = {'User-Agent': 'Custom'}
+
+    response = requests.get(url.format(subreddit))
+    data = response.json()
+
+    if response.status_code != 200:
+        return 0
+    else:
+        return data["data"]["subscribers"]
